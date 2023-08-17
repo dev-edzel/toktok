49b2a3a17095861451fa0bff8f90378a5f35424c
Edzel <dev.edzeldacanay@gmail.com>
2023-08-17 18:00:17 +0800

    updated

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -9,22 +9,46 @@
                 </NuxtLink>
             </div>
 
-            <div class="hidden md:flex items-center bg-[#F1F1F1] p-1 rounded-full max-w-[380px] w-full ">
-                <input type="text" class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none" placeholder="Search Accounts">
-            <div class="px-3 py-1 flex items-center border-l border-l-gray-300">
-                <Icon name="ri-search-line" color="A1A2A7" size="22" /> 
-            </div>
-            </div>
+                <div class="hidden md:flex items-center bg-[#F1F1F1] p-1 rounded-full max-w-[380px] w-full ">
+                    <input type="text" class="w-full pl-3 my-2 bg-transparent placeholder-[#838383] text-[15px] focus:outline-none" placeholder="Search Accounts">
+                <div class="px-3 py-1 flex items-center border-l border-l-gray-300">
+                    <Icon name="ri-search-line" color="A1A2A7" size="22" /> 
+                </div>
+                </div>
+
             <div class="flex items-center justify-end gap-3 min-2-[275px] max-w-[320px] w-full">
-                <button class="flex item-center border rounded-sm px-2 py-[6px] hover:bg-gray-100">
-                    <Icon name="mdi:plus" color="#000000" size="22" />
-                    <span class="px-2 font-medium text-[15px]"> Upload </span>
-                </button>
-            <div class="flex items-center">
-                <button class="flex items-center bg-[#6654F4] text-white border rounded-md px-3 py-[6px]">
-                    <span class="mx-4 font-medium text-[15px]">Log In</span>
-                </button>
-            </div>
+                    <button class="flex item-center border rounded-sm px-2 py-[6px] hover:bg-gray-100">
+                        <Icon name="mdi:plus" color="#000000" size="22" />
+                        <span class="px-2 font-medium text-[15px]"> Upload </span>
+                    </button>
+                    
+                <div v-if="false" class="flex items-center">
+                    <button class="flex items-center bg-[#6654F4] text-white border rounded-md px-3 py-[6px]">
+                        <span class="mx-4 font-medium text-[15px]">Log In</span>
+                    </button>
+                    <Icon name="mdi:dots-vertical" color="#161724" size="25"/>
+                </div>
+                
+                <div class="flex items-center">
+                    <Icon class="ml-1 mr-4" name="carbon:send-alt" color="#161724" size="30"/>
+                    <Icon class="mr-4" name="bx:message-detail" color="#161724" size="27"/>
+                    <div class="relative">
+                        <button class="mt-1" @click="$event => showMenu = !showMenu">
+                            <img class="rounded-full" width="33" src="https://socialcounts.org/_next/image?url=https%3A%2F%2Fwww.banner.yt%2FUCX8foYPDD2KVKhkGPpMSUCA%2Favatar%3Fwidth%3D640&w=256&q=75">
+                        </button>
+
+                        <div v-if="showMenu" id="PopupMenu" class="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[43px] -right-2">
+                            <NuxtLink @click="$event => showMenu = false" class="flex items-center justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
+                                <Icon name="ph:user" size="20"/>
+                                <span class="pl-2 font-semibold text-sm">Profile</span>
+                            </NuxtLink>
+                            <div class="flex items-center justify-start py-3 px-1.5 hover:bg-gray-100 border-t cursor-pointer">
+                                <Icon name="ic-outline-login" size="20"/>
+                                <span class="pl-2 font-semibold text-sm">Log Out</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -32,4 +56,5 @@
 
 <script setup>
     const route = useRoute()
+    let showMenu = ref(false)
 </script>
