5cf1cd42a004127c0bea1bc648d519138eb766d8
Edzel <dev.edzeldacanay@gmail.com>
2023-08-22 16:09:28 +0800

    updated

---
+++ pages/profile/[id].vue
@@ -0,0 +1,26 @@
+<template>
+    <MainLayout>
+        <div class="pt-[90px] 2xl:pl-[185px] lg:pl-[160px] lg:pr-0 pr-2 w-[calc(100%-90px)] max-w-[1800px] 2xl:mx-auto">
+            <div class="flex w-[calc(100vw-230px)]">
+                <img class="max-w-[120px] rounded-full" src="https://socialcounts.org/_next/image?url=https%3A%2F%2Fwww.banner.yt%2FUCX8foYPDD2KVKhkGPpMSUCA%2Favatar%3Fwidth%3D640&w=256&q=75">
+                <div class="ml-5 w-full">
+                <div class="text-[30px] font-bold truncate">
+                    User name
+                </div>
+                <div class="text-[18px] truncate">User name</div>
+                <button v-if="true" class="flex item-center rounded-md py-1.5 px-3.5 mt-3 text-[15px] font-semibold border hover:bg-gray-100"> 
+                    <Icon class="mt-0.5 mr-1" name="mdi:pencil" size="18" />
+                    <div>Edit Profile</div>
+                </button>
+                <button v-else class="flex items-center rounded-md py-1.5 px-8 mt-3 text-[15px] text-white font-semibold bg-[#6654F4]">
+                    Follow
+                </button>
+            </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+</script>
--- pages/upload/index.vue
+++ pages/upload/index.vue
@@ -1,57 +1,131 @@
 <template>
-        <UploadLayout>
-            <div class="w-full mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
-                <div>
-                    <div class="text-[23px] font-semibold">Upload Video</div>
-                    <div class="text-gray-400 mt-1">Post Video </div>
+
+    <UploadError :errorType="errorType" />
+
+    <UploadLayout>
+        <div class="w-full mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
+            <div>
+                <div class="text-[23px] font-semibold">Upload Video</div>
+                <div class="text-gray-400 mt-1">Post Video </div>
+            </div>
+
+            <div class="mt-8 md:flex gap-6">
+                
+                <label v-if="!fileDisplay" @drop.prevent="onDrop" @dragover.prevent="" for="fileInput" 
+                    class="md:mx-0 mx-ato mt-4 mb-6 flex flex-col items-center justify-center w-full max-w-[260px] h-[470px] 
+                    text-center p-3 border-2 border-dashed border-gray-300 rounded-lg hover:bg-gray-100 cursor-pointer">
+
+                    <Icon name="majesticons:cloud-upload" size="50" color="b3b3b1"></Icon>
+                    <div class="mt-4 text-[17px]">Select video to upload</div>
+                    <div class="mt-1.5 text-gray-500 text-[13px]">Drag and drop files</div>
+                    <div class="mt-1.5 text-gray-500 text-[13px]">Long videos can be split into multiple parts to get more exposure</div>
+                    <div class="mt-12 text-gray-400 text-sm">Support mp4</div>
+                    <div class="mt-1 text-gray-400 text-[13px]">Up to 600 seconds</div>
+                    <div class="mt-1 text-gray-400 text-[13px]">Less than 2 GB</div>
+                    <div class="mt-1 text-gray-400 text-[13px]">Less than 30 videos</div>
+                    <div class="px-2 py-1.5 mt-8 text-white text-[15px] w-[80%] bg-[#6654F4] rounded-sm"> Select files</div>
+                    <input ref="file" type="file" id="fileInput" @input="onChange" hidden accept=".mp4">
+                </label>
+                <div v-else class="md:mx-0 mx-auto mt-4 md:mb-12 mb-16 flex items-center justify-center w-full max-w-[260px] h-[540px] p-3 rounded-2xl cursor-pointer relative">
+                    <div class="bg-black h-full w-full" />
+
+                    <img class="absolute z-20 pointer-events-none" src="~/assets/images/mobile-case.png">
+                    <video autoplay loop muted :src="fileDisplay" height="100"></video>
+                    <div class="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
+                        <div class="flex  items-center truncate">
+                            <Icon name="clarity:success-standard-line" size="16" class="min-w-[16px]" />
+                            <div class="text-[11px] pl-1 truncate text-ellipsis">{{ fileData.name }}</div>
+                        </div>
+                        <button @click="$event=>clearVideo()" class="text-[11px] ml-2 font-semibold"> Change </button>
+                    </div>
                 </div>
 
-                <div class="mt-8 md:flex gap-6">
-                    <label v-if="false" for="fileInput" class="md:mx-0 mx-ato mt-4 mb-6 flex flex-col items-center justify-center w-full max-w-[260px] h-[470px] text-center p-3 border-2 border-dashed border-gray-300 rounded-lg hover:bg-gray-100 cursor-pointer">
-                        <Icon name="majesticons:cloud-upload" size="50" color="b3b3b1"></Icon>
-                        <div class="mt-4 text-[17px]">Select video to upload</div>
-                        <div class="mt-1.5 text-gray-500 text-[13px]">Drag and drop files</div>
-                        <div class="mt-1.5 text-gray-500 text-[13px]">Long videos can be split into multiple parts to get more exposure</div>
-                        <div class="mt-12 text-gray-400 text-sm">Support mp4</div>
-                        <div class="mt-1 text-gray-400 text-[13px]">Up to 600 seconds</div>
-                        <div class="mt-1 text-gray-400 text-[13px]">Less than 2 GB</div>
-                        <div class="mt-1 text-gray-400 text-[13px]">Less than 30 videos</div>
-                        <div class="px-2 py-1.5 mt-8 text-white text-[15px] w-[80%] bg-[#6654F4] rounded-sm"> Select files</div>
-                        <input ref="file" type="file" id="fileInput" hidden accept=".mp4">
-                    </label>
-                    <div v-else class="md:mx-0 mx-auto mt-4 md:mb-12 mb-16 flex items-center justify-center w-full max-w-[260px] h-[540px] p-3 rounded-2xl cursor-pointer relative">
-                        <div class="bg-black h-full w-full" />
-
-                        <img class="absolute z-20 pointer-events-none" src="~/assets/images/mobile-case.png">
-                        <video autoplay loop muted src="~/assets/videos/toktok.mp4" height="100"></video>
-                        <div class="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
-                            <div class="flex  items-center truncate">
-                                <Icon name="clarity:success-standard-line" size="16" class="min-w-[16px]" />
-                                <div class="text-[11px] pl-1 truncate text-ellipsis">video name</div>
-                            </div>
-                            <button class="text-[11px] ml-2 font-semibold"> Change </button>
+                <div class="mt-4 mb-6">
+                    <div class="flex bg-[#F8F8F8] py-4 px-6">
+                        <div>
+                            <Icon class="mr-4" size="20" name="mdi:box-cutter-off" />
                         </div>
+                        <div>
+                            <div class="text-semibold text-[15px] mb-1.5">Divide videos and edit</div>
+                            <div class="text-semibold text-[13px] text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                                Sit sunt nisi, quo adipisci voluptatem sed deleniti doloremque atque</div>
                     </div>
-
-                    <div class="mt-4 mb-6">
-                        <div class="flex bg-[#F8F8F8] py-4 px-6">
-                            <div>
-                                <Icon class="mr-4" size="20" name="mdi:box-cutter-off" />
-                            </div>
-                            <div>
-                                <div class="text-sembold text-[15px] mb-1.5">Divide videos and edit</div>
-                                <div class="text-semibold text-[13px] text-gray-400">Divide</div>
-                            </div>
-                        </div>
                         <div class="flex justify-end max-w-[130px] w-full h-full text-center my-auto">
-                            <button class="px-8 py-1.5 text-white text-[15px] bg-[#6654F4] rounded-sm"> Edit </button>
-                        </div>  
+                            <button class="px-8 py-1.5 text-white bg-[#6654F4] rounded-sm">
+                                Edit
+                            </button>
+                        </div>
                     </div>
-                </div> 
-            </div>
-        </UploadLayout>
+                    <div class="mt-5">
+                        <div class="flex items-center justify-between">
+                            <div class="mb-1 text-[15px]">Caption</div>
+                            <div class="text-gray-400 text-[12px]">{{ caption.length }}/150</div>
+                        </div>
+                        <input v-model="caption" type="text" maxlength="150" class="w-full border p-2.5 rounded-md focus:outline-none">
+                    </div>
+                    <div class="flex gap-3">
+                        <button @click="$event => discard()" class="px-10 py-2.5 mt-8 border text-[16px] hover:bg-gray-100 rounded-sm">
+                            Discard
+                        </button>
+                        <button class="px-10 py-2.5 mt-8 border text-[16px] text-white bg-[#6654F4] rounded-sm">
+                            Post
+                        </button>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    </UploadLayout>
 </template>
 
 <script setup>
 import UploadLayout from '~/layouts/UploadLayout.vue';
+
+let file = ref(null)
+let fileDisplay = ref(null)
+let errorType = ref(null)
+let caption = ref('')
+let fileData = ref(null)
+let errors = ref(null)
+let isUploading = ref(false)
+
+watch(() => caption.value, (caption) => {
+    if (caption.length >= 150) {
+        errorType.value = 'caption'
+        return
+    }
+    errorType.value = null
+})
+
+const onChange = () => {
+    fileDisplay.value = URL.createObjectURL(file.value.files[0])
+    fileData.value = file.value.files[0]
+}
+
+const onDrop = (e) => {
+    errorType.value = ''
+    file.value = e.dataTransfer.files[0];
+    fileData.value = e.dataTransfer.files[0]
+
+    let extension = file.value.name.substring(file.value.name.lastIndexOf('.') + 1)
+
+    if (extension !== 'mp4') {
+        errorType.value = 'file'
+        return
+    }   
+
+    fileDisplay.value = URL.createObjectURL(e.dataTransfer.files[0])
+}
+
+const discard = () => {
+    file.value = null
+    fileDisplay.value = null
+    fileData.value = null
+    caption.value = ''
+}
+
+const clearVideo = () => {
+    file.value = null
+    fileDisplay.value = null
+    fileData.value = null
+}
 </script>
