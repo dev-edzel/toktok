c5da9b4f167976d5437cf04b17a7b3323f25939a
Edzel <dev.edzeldacanay@gmail.com>
2023-08-23 10:26:33 +0800

    added profile

--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -15,7 +15,53 @@
                 <button v-else class="flex items-center rounded-md py-1.5 px-8 mt-3 text-[15px] text-white font-semibold bg-[#6654F4]">
                     Follow
                 </button>
+                </div>
             </div>
+
+            <div class="flex items-center pt-4">
+                <div class="mr-4">
+                    <span class="font-bold">69.9K</span>
+                    <span class="text-gray-500 font-light text-[15px] pl-1.5">Following</span>
+                </div>
+                <div class="mr-4">
+                    <span class="font-bold">69.9K</span>
+                    <span class="text-gray-500 font-light text-[15px] pl-1.5">Followers</span>
+                </div>
+                <div class="mr-4">
+                    <span class="font-bold">69.9K</span>
+                    <span class="text-gray-500 font-light text-[15px] pl-1.5">Likes</span>
+                </div>
+            </div>
+
+            <div class="pt-4 mr-4 text-gray-500 font-light text-[15px] pl-1.5 max-w-[500px]">
+                This is the bio section
+            </div>
+
+            <div class="w-full flex items-center pt-4 border-b">
+                <div class="w-60 text-center py-2 text-[17px] font-semibold border-b-2 border-b-black">Videos</div>
+                <div class="w-60 text-gray-500 text-center py-2 text-[17px] font-semibold">
+                    <Icon name="material-symbols:lock-open" class="mb-0.5" /> Liked
+                </div>
+            </div>
+            <div class="mt-4 grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
+                <PostUser />
             </div>
         </div>
     </MainLayout>
